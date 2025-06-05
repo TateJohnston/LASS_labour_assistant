f9d56b387e8cf3daf2a7c21284e5f57842ed612a
TateJohnston <tate1johnston@gmail.com>
2025-06-05 22:29:25 +0800

    licenses page complete

--- backend/controllers/licensesController.js
+++ backend/controllers/licensesController.js
@@ -1,6 +1,6 @@
 const { json } = require("sequelize");
 const Models = require("../models");
-const { sequelize } = require("../models/employees");
+const { sequelize, update } = require("../models/employees");
 
 const getLicenses = (req, res) => {
   Models.Licenses.findAll({})
@@ -30,7 +30,7 @@ const getDueLicenses = (req, res) => {
       type: sequelize.QueryTypes.SELECT,
     })
     .then((data) => {
-      if (data.length > 1) {
+      if (data.length > 0) {
         res.send({ result: 200, data: data });
       } else {
         res.send({
@@ -44,7 +44,58 @@ const getDueLicenses = (req, res) => {
     });
 };
 
+const updateLicense = (req, res) => {
+  const licenseID = req.params.licenseID;
+  const cost_per_month = req.body.cost_per_month;
+  const expiry = req.body.expiry;
+
+  if (cost_per_month && expiry) {
+    Models.Licenses.update(
+      {
+        cost_per_month: cost_per_month,
+        expiry: expiry,
+      },
+      { where: { license_id: licenseID } }
+    )
+      .then((data) => {
+        res.send({ result: 200, data: data });
+      })
+      .catch((err) => {
+        res.status(500).send({ result: 500, error: err.message });
+      });
+  } else if (!cost_per_month && expiry) {
+    Models.Licenses.update(
+      {
+        expiry: expiry,
+      },
+      { where: { license_id: licenseID } }
+    )
+      .then((data) => {
+        res.send({ result: 200, data: data });
+      })
+      .catch((err) => {
+        res.status(500).send({ result: 500, error: err.message });
+      });
+  } else if (cost_per_month && !expiry) {
+    Models.Licenses.update(
+      {
+        cost_per_month: cost_per_month,
+      },
+      { where: { license_id: licenseID } }
+    )
+      .then((data) => {
+        res.send({ result: 200, data: data });
+      })
+      .catch((err) => {
+        res.status(500).send({ result: 500, error: err.message });
+      });
+  } else {
+    res.send({ result: 404, message: "Nothing to update" });
+  }
+};
+
 module.exports = {
   getLicenses,
   getDueLicenses,
+  updateLicense,
 };
